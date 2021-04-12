55cbba9889801c840a0b059029b56ca6a137a2a6
michel <michel.cavaud@gmail.com>
2021-04-12 18:31:45 +0200

    Ajout page type

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,13 +2,26 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController{
-    
-     protected $data;
-     
-     
-    public function index(){
-        $this->data['titre'] = 'Adecio';
-        $this->twig->display('home.html', $this->data);
+class Home extends BaseController {
+
+    protected $data;
+
+    public function index($page = '', $souspage = '') {
+        switch ($page) {
+            case '':
+                $this->data['titre'] = 'Adecio';
+                $this->twig->display('home.html', $this->data);
+                break;
+            case 'coachingentreprise':
+                switch ($souspage) {
+                    case 'coachingindividuel':
+                        $this->data['titre'] = 'Adecio';
+                        $this->twig->display('type.html', $this->data);
+                        break;
+                }
+        }
     }
+
+    
+
 }
